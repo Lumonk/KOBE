1cc892ffb565656b15449c3282d587cd7661ae84
lujinming <lujinming_556@163.com>
2019-08-03 22:52:25 +0800

    extract know_dict and store as pkl

--- core/utils/knowledge_split.py
+++ core/utils/knowledge_split.py
@@ -6,74 +6,84 @@ import codecs
 import jieba
 import jieba.posseg
 import jieba.analyse
+import pickle
 
 
-s = "欧 美 是 世 界 上 的 一 块 地 域 ， 其 文 化 属 于 西 方 文 化 ， 是 欧 洲 和 北 美 的 统 称 。 "
-    # "包 英 文 B a g ， 即 手 袋 箱 包 ， 包 括 钱 包 、 钥 匙 包 、 零 钱 包 、 手 拿 包 、 拎 包 、 背 包 、 书 包 、 挎 包 、 公 文 包 等 等 。 真 皮 ， 位 于 表 皮 深 层 ， 向 下 与 皮 下 组 织 相 连 ， 真 皮 结 缔 组 织 的 胶 原 纤 维 和 弹 性 纤 维 互 相 交 织 在 一 起 ， 埋 于 基 质 内 。 手 包 简 介 手 提 的 较 小 的 包 包 ， 多 用 皮 革 制 成 ， 分 为 男 款 和 女 款 。 "
-s2 = "手 包 女 手 拿 包 2 0 1 8 新 款 手 抓 包 韩 版 真 皮 个 性 欧 美 百 搭 信 封 气 质 包 包 潮"
+# extract keys dict
+def create_keys_dict():
+    all_keys = {'女装'}
+    with codecs.open(r'C:\Users\jmlu\Desktop\Kobe\KOBE\data\origin_data\alldata.txt', 'r', 'utf-8') as f:
+        while True:
+            pline = f.readline()
 
-s = ''.join(s.split(' '))
-s2 = ''.join(s2.split(' '))
+            if pline == '':
+                break
+            keys = set(pline.strip().split('\t')[0].split(' '))
+            all_keys = all_keys|keys
 
-print('='*40)
-print('1. 分词')
-print('-'*40)
+    all_keys = list(all_keys)
 
-seg_list = jieba.cut(s, cut_all=True)
-print("Full Mode: " + "/ ".join(seg_list))  # 全模式
+    with codecs.open(r'C:\Users\jmlu\Desktop\Kobe\KOBE\data\keys_dict.txt', 'w', 'utf-8') as f:
+        for key in all_keys:
+            f.write(key)
+            f.write('\n')
 
-seg_list = jieba.cut(s2, cut_all=True)
-print("Full Mode: " + "/ ".join(seg_list))  # 全模式
+#
+def create_know_pair():
+    freqwords=[]
+    with codecs.open(r'C:\Users\jmlu\Desktop\Kobe\KOBE\500freqword.str', 'r', 'utf-8') as f:
+        while True:
+            pline = f.readline()
 
-seg_list = jieba.cut(s, cut_all=False)
-print("Default Mode: " + "/ ".join(seg_list))  # 默认模式
+            if pline == '':
+                break
+            freqwords.append(pline.strip())
+    print(len(freqwords))
+    #
+    jieba.load_userdict(r"C:\Users\jmlu\Desktop\Kobe\KOBE\data\keys_dict.txt")
+    knowledge_dict = {}
+    line_num = 0
+    with codecs.open(r'C:\Users\jmlu\Desktop\Kobe\KOBE\data\aspect-user\preprocessed\train.supporting_facts_str', 'r', 'utf-8') as f:
+        while True:
+            pline = f.readline()
 
-seg_list = jieba.cut(s)
-print(", ".join(seg_list))
+            if pline == '':
+                break
+            pline = ''.join(pline.strip().split(' '))
+            sentences = pline.split('。')
+            for s in sentences:
+                words = jieba.posseg.cut(s)
+                for word, flag in words:
+                    if (word in freqwords):
+                        knowledge_dict[word] = s+'。'
+                        # print(word, s)
+                        break
+            line_num+= 1
+            if line_num %100 == 0:
+                print(line_num)
 
-seg_list = jieba.cut_for_search(s)  # 搜索引擎模式
-print(", ".join(seg_list))
+            # break
+    print(len(knowledge_dict))
+    with codecs.open(r'C:\Users\jmlu\Desktop\Kobe\KOBE\data\know_dict.txt', 'w', 'utf-8') as f:
+        for key, know in knowledge_dict.items():
+            f.write(key)
+            f.write('\t')
+            f.write(know)
+            f.write('\n')
 
+def create_know_dict():
+    know_dict = {}
+    with codecs.open(r'C:\Users\jmlu\Desktop\Kobe\KOBE\data\know_dict.txt', 'r', 'utf-8') as f:
+        while True:
+            pline = f.readline()
 
-print('='*40)
-print('3. 关键词提取')
-print('-'*40)
-print(' TF-IDF')
-print('-'*40)
+            if pline == '':
+                break
+            pline = (pline.strip().split('\t'))
+            know_dict[pline[0]] = pline[1]
+            # print(know_dict)
 
-#s = "此外，公司拟对全资子公司吉林欧亚置业有限公司增资4.3亿元，增资后，吉林欧亚置业注册资本由7000万元增加到5亿元。吉林欧亚置业主要经营范围为房地产开发及百货零售等业务。目前在建吉林欧亚城市商业综合体项目。2013年，实现营业收入0万元，实现净利润-139.13万元。"
-for x, w in jieba.analyse.extract_tags(s, withWeight=True):
-    print('%s %s' % (x, w))
-
-print('-'*40)
-print(' TextRank')
-print('-'*40)
-
-for x, w in jieba.analyse.textrank(s, withWeight=True):
-    print('%s %s' % (x, w))
-
-print('='*40)
-print('4. 词性标注')
-print('-'*40)
-
-words = jieba.posseg.cut(s)
-for word, flag in words:
-    print('%s %s' % (word, flag))
-
-print('='*40)
-print('6. Tokenize: 返回词语在原文的起止位置')
-print('-'*40)
-print(' 默认模式')
-print('-'*40)
-
-result = jieba.tokenize('永和服装饰品有限公司')
-for tk in result:
-    print("word %s\t\t start: %d \t\t end:%d" % (tk[0],tk[1],tk[2]))
-
-print('-'*40)
-print(' 搜索模式')
-print('-'*40)
-
-result = jieba.tokenize('永和服装饰品有限公司', mode='search')
-for tk in result:
-    print("word %s\t\t start: %d \t\t end:%d" % (tk[0],tk[1],tk[2]))
+    fw = open(r'C:\Users\jmlu\Desktop\Kobe\KOBE\data\know_dict.pkl', "wb")
+    print(fw)
+    pickle.dump(know_dict, fw)
+    fw.close()
